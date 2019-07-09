dc0f4834cdf074002915e591c3884be5497b305d
hycner <hycner@users.noreply.github.com>
2019-07-09 11:41:19 -0700

    work in i18n

---
+++ src/lib/intl/index.ts
@@ -0,0 +1,7 @@
+import enUS from './enUS'
+import jaJP from './jaJP'
+
+export default {
+  'en-US': enUS,
+  'ja-JP': jaJP,
+}
---
+++ src/lib/intl/jaJP.ts
@@ -0,0 +1,44 @@
+export default {
+  address: {
+    city: '都市', // 'シティ'
+    clear: '消す', // 'クリア'
+    country: '国',
+    email: '電子メイル', // 'Eメール'
+    name: '名',
+    nothing: '何も',
+    phone: '電話',
+    save: '保存', // 'セーブ'
+    state: '県',
+    street1: '通り一', // '通り1', '道1', '道一'
+    street2: '通り二', // '通り2', '道2', '道二'
+    zip: '郵便番号',
+  },
+  creditCard: {
+    clear: '消す', // 'クリア'
+    cvv: 'CVV番号', // 'CVV'
+    expDate: '有効期限',
+    expMonth: '有効期限月',
+    expYear: '有効期限年',
+    name: '名',
+    nothing: '何も',
+    number: '数',
+    save: '保存', // 'セーブ'
+  },
+  login: {
+    accountId: '口座ID', // 'アカウントID'
+    clear: '消す', // 'クリア'
+    nothing: '何も',
+    password: '合言葉', // 'パスワード'
+    submit: '提出',
+    username: 'ユーザー名', // '名前'
+  },
+  passwordReset: {
+    clear: '消す', // 'クリア'
+    email: '電子メイル', // 'Eメール'
+    confirmNewPass: '新しいパスワードを確認',
+    newPass: '新しいパスワード',
+    nothing: '何も',
+    oldPass: '現在のパスワード',
+    submit: '提出',
+  },
+}
